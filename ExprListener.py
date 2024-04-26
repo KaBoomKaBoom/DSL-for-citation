0c9ecd47b623c803ab80bde6be000a45256a66e0
Berco Andrei <andrei2003berco@gmail.com>
2024-04-26 18:09:37 +0300

    listener: add method for mla cites

--- ExprListener.py
+++ ExprListener.py
@@ -12,7 +12,9 @@ from docx.shared import Pt
 
 # This class defines a complete listener for a parse tree produced by ExprParser.
 class ExprListener(ParseTreeListener):
-
+    
+    def __init__(self):
+        self.var_value = None
     # Enter a parse tree produced by ExprParser#program.
     def enterProgram(self, ctx:ExprParser.ProgramContext):
         pass
@@ -51,7 +53,23 @@ class ExprListener(ParseTreeListener):
 
     # Enter a parse tree produced by ExprParser#statement.
     def enterStatement(self, ctx:ExprParser.StatementContext):
-        pass
+        # print("Entering statement")
+        queue=[child.getText() for child in ctx.children]
+        # print(queue)
+        if ctx.var_assign():
+            for var_assign_ctx in ctx.var_assign():
+                self.var_value = self.enterVar_assign(var_assign_ctx)
+            #print(f"Variable: {self.var_value}")
+        # if "for" in queue:
+        #     #print(self.var_value)
+        #     if any(el in queue[queue.index("in")+5] for el in ['CiteAPA' , 'CiteMLA' , 'CiteCMS' , 'CiteCSE' , 'CiteISO' , 'CiteIEEE'] ):
+        #         for q in self.var_value:
+        #             self.enterExpr(ctx.expr())
+        pass         
+
+
+            
+        
 
     # Exit a parse tree produced by ExprParser#statement.
     def exitStatement(self, ctx:ExprParser.StatementContext):
@@ -69,6 +87,7 @@ class ExprListener(ParseTreeListener):
     def enterExpr(self, ctx:ExprParser.ExprContext):
         pass
 
+
     # Exit a parse tree produced by ExprParser#expr.
     def exitExpr(self, ctx:ExprParser.ExprContext):
         pass
@@ -80,28 +99,38 @@ class ExprListener(ParseTreeListener):
         method_args = [arg.getText() for arg in ctx.id_()]
         for arg in ctx.literal():
             method_args.append(arg.getText().replace('"', ''))
-        if method_name == "CiteMLA":
-            cite_text,cite_bib = generate_apa_citation(1, *method_args)
+        if method_name == "CiteAPA":
+            cite_text,cite_bib = generate_apa_citation(*method_args)
+            # print(f"Citation: {cite_text}")
+            # print(f"Bibliography: {cite_bib}")
+            return cite_text, cite_bib
+        elif method_name == "CiteMLA":
+            cite_text,cite_bib = generate_mla_citation(*method_args)
             print(f"Citation: {cite_text}")
             print(f"Bibliography: {cite_bib}")
+            return cite_text, cite_bib
+
 
     # Exit a parse tree produced by ExprParser#method_call.
     def exitMethod_call(self, ctx:ExprParser.Method_callContext):
-        print("Exiting method call")
+        #print("Exiting method call")
+        pass
 
 
 
     # Enter a parse tree produced by ExprParser#var_assign.
     def enterVar_assign(self, ctx:ExprParser.Var_assignContext):
-        print("Entering variable assignment")
-        var_name = ctx.id_().getText()
+        #print("Entering variable assignment")
+        #var_name = ctx.id_().getText()
         var_value = [string.getText() for string in ctx.string_literal()]
-        print(f"Variable: {var_name}, Value: {var_value}")
+        var_value = [el.replace('"', '') for el in var_value]  
+        #print(f"Variable: {var_name}, Value: {var_value}")
+        return var_value
 
     # Exit a parse tree produced by ExprParser#var_assign.
     def exitVar_assign(self, ctx:ExprParser.Var_assignContext):
-        print("Exiting variable assignment")
-
+        #print("Exiting variable assignment")
+        pass
 
     # Enter a parse tree produced by ExprParser#math_expr.
     def enterMath_expr(self, ctx:ExprParser.Math_exprContext):
@@ -192,16 +221,16 @@ class ExprListener(ParseTreeListener):
     def exitString_literal(self, ctx:ExprParser.String_literalContext):
         pass
 
-def generate_apa_citation(index, qouote, source_type, author, year, title, book_title, publisher):
+def generate_apa_citation(qouote, source_type, author, year, title, book_title, publisher):
     #print(qouote, source_type, author, year, title, book_title, publisher)
-    doc = Document()
+    doc = Document("test.docx")
         # For in-text citation
     citation_text = qouote + "(" + author + ", " + str(year) + ")"
     p = doc.add_paragraph(citation_text)
     p.style.font.name = 'Times New Roman'
     p.style.font.size = Pt(12)
         # For bibliography/reference list
-    citation_bib = f"[{index}]  "+ author + " (" + str(year) + "). " + title + ". "
+    citation_bib = author + " (" + str(year) + "). " + title + ". "
     p = doc.add_paragraph(citation_bib)
 
     if source_type == "book":
@@ -214,6 +243,37 @@ def generate_apa_citation(index, qouote, source_type, author, year, title, book_
         p.add_run(publisher + ".").italic = True 
     elif source_type == "website":
         citation_bib += book_title  + ' ' + publisher + "."
+    doc.save('test.docx')
+    return citation_text, citation_bib
+
+def generate_mla_citation(qouote, source_type, author, year, title, book_title, publisher):
+    #print(qouote, source_type, author, year, title, book_title, publisher)
+    doc = Document("test.docx")
+        # For in-text citation
+    citation_text = qouote + " (" + author + ")."
+    p = doc.add_paragraph(citation_text)
+    p.style.font.name = 'Times New Roman'
+    p.style.font.size = Pt(12)
+        # For bibliography/reference list
+    citation_bib = author + '"' + title + '."'
+    p = doc.add_paragraph(citation_bib)
+
+    if source_type == "book":
+        citation_bib += book_title  + ', ' + publisher + ', ' + str(year) +  "."
+        p.add_run(book_title + ', ').italic = True
+        p.add_run(publisher + ", ")
+        p.add_run(str(year) + ".") 
+    elif source_type == "journal":
+        citation_bib += book_title  + ', ' + publisher + ', ' + str(year) +  "."
+        p.add_run(book_title + ', ').italic = True
+        p.add_run(publisher + ", ")
+        p.add_run(str(year) + ".")
+         
+    elif source_type == "website":
+        citation_bib += book_title  + ', ' + publisher + ', ' + str(year) +  "."
+        p.add_run(book_title + ', ').italic = True
+        p.add_run(publisher + ", ")
+        p.add_run(str(year) + ".")
     doc.save('test.docx')
     return citation_text, citation_bib
 
